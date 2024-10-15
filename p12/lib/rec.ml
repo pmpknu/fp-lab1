9bb925b30fb3db01eaca1f206d88e3c32df2b5bc
pmpknu <dangorlyakov@gmail.com>
2024-10-15 23:26:11 +0300

    fix(p12): count_factors changed to non-tail rec
    	- count_factors in rec.ml changed from tail recursion implementation to non-tail recursion

--- p12/lib/rec.ml
+++ p12/lib/rec.ml
@@ -2,12 +2,14 @@ let triangle_number n =
   n * (n + 1) / 2
 
 let count_factors n =
-  let rec aux i count =
-    if i * i > n then count
-    else if n mod i = 0 then aux (i + 1) (count + if i * i = n then 1 else 2)
-    else aux (i + 1) count
+  let rec aux i =
+    if i * i > n then 0
+    else if n mod i = 0 then
+      if i * i = n then 1 + aux (i + 1)  
+      else 2 + aux (i + 1)               
+    else aux (i + 1)
   in
-  if n <= 0 then 0 else aux 1 0
+  if n <= 0 then 0 else aux 1
 
 let rec find_triangular_with_divisors target n =
   let tri_num = triangle_number n in
