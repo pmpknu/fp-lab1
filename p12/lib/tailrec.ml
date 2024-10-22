03b1de12bf640d1008a596697f20bed117afcf3b
pmpknu <dangorlyakov@gmail.com>
2024-10-22 10:06:32 +0300

    fix(p12) small fixes

--- p12/lib/tailrec.ml
+++ p12/lib/tailrec.ml
@@ -1,4 +1,4 @@
-let count_factors n =
+let count_factors_tail n =
   let rec aux i count =
     if i * i > n then count
     else if n mod i = 0 then aux (i + 1) (count + if i * i = n then 1 else 2)
@@ -6,12 +6,12 @@ let count_factors n =
   in
   if n <= 0 then 0 else aux 1 0
 
-let find_triangular_with_divisors target =
+let find_triangular_with_divisors_tail target =
   let rec aux n tri_num =
-    let factors_count = count_factors tri_num in
+    let factors_count = count_factors_tail tri_num in
     if factors_count > target then tri_num
     else aux (n + 1) (tri_num + n + 1)
   in
   aux 1 1
 
-let result = find_triangular_with_divisors 500
+let result = find_triangular_with_divisors_tail 500
