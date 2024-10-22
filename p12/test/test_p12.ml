ed36abe6561ed26439b6b1b3b7c13ab7999430b5
pmpknu <dangorlyakov@gmail.com>
2024-10-22 17:40:45 +0300

    tests(p12) add iterative tests

--- p12/test/test_p12.ml
+++ p12/test/test_p12.ml
@@ -3,13 +3,13 @@ open P12_lib
 
 (* Tailrec tests *)
 let test_tailrec_count_factors _ =
-  OUnit2.assert_equal 6 (Tailrec.count_factors_tail 28);
-  OUnit2.assert_equal 9 (Tailrec.count_factors_tail 36);
-  OUnit2.assert_equal 0 (Tailrec.count_factors_tail 0)
+  assert_equal 6 (Tailrec.count_factors_tail 28);
+  assert_equal 9 (Tailrec.count_factors_tail 36);
+  assert_equal 0 (Tailrec.count_factors_tail 0)
 
 let test_tailrec_find_triangular _ =
-  OUnit2.assert_equal 28 (Tailrec.find_triangular_with_divisors_tail 5);
-  OUnit2.assert_equal 76576500 (Tailrec.find_triangular_with_divisors_tail 500)
+  assert_equal 28 (Tailrec.find_triangular_with_divisors_tail 5);
+  assert_equal 76576500 (Tailrec.find_triangular_with_divisors_tail 500)
 
 (* Rec tests *)
 let test_rec_count_factors _ =
@@ -20,13 +20,26 @@ let test_rec_count_factors _ =
 let test_rec_find_triangular _ =
   assert_equal 28 (Rec.find_triangular_with_divisors 5 1);
   assert_equal 76576500 (Rec.find_triangular_with_divisors 500 1)
+
+(* Iterative tests *)
+let test_iterative_count_divisors _ =
+  assert_equal 6 (Iterative.count_divisors 28);
+  assert_equal 9 (Iterative.count_divisors 36);
+  assert_equal 0 (Iterative.count_divisors 0)
+
+let test_iterative_find_triangle _ =
+  assert_equal 28 (Iterative.find_triangle_with_divisors 5);
+  assert_equal 76576500 (Iterative.find_triangle_with_divisors 500)
+
 let suite =
   "Project Euler Problem 12 Tests" >::: [
     "Tailrec - count_factors" >:: test_tailrec_count_factors;
     "Tailrec - find_triangular" >:: test_tailrec_find_triangular;
     "Rec - count_factors" >:: test_rec_count_factors;
     "Rec - find_triangular" >:: test_rec_find_triangular;
-]
+    "Iterative - count_factors" >:: test_iterative_count_divisors;
+    "Iterative - find_triangular" >:: test_iterative_find_triangle;
+    ]
 
 let _ = run_test_tt_main suite
   
