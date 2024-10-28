7973a0015279228a5ecbfc00adbad01ad4cee8f0
pmpknu <dangorlyakov@gmail.com>
2024-10-28 09:59:13 +0300

    feat(p19): add days_in_month function
    	- function returns amount of days in month according to year

--- p19/lib/date.ml
+++ p19/lib/date.ml
@@ -1,2 +1,9 @@
 let is_leap year =
   (year mod 4 = 0 && year mod 100 <> 0) || (year mod 400 = 0)
+
+let days_in_month year month =
+  match month with
+  | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31
+  | 4 | 6 | 9 | 11 -> 30
+  | 2 -> if is_leap year then 29 else 28
+  | _ -> failwith "Invalid month"
